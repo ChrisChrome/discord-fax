317b668ef2b055ecf087655b81f60fa05471df23
ChrisChrome <christophercookman@gmail.com>
2023-03-01 20:00:39 -0700

    IT'S ALIVEEEE!!!

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const rest = new REST({
 const fs = require("fs")
 const path = require("path")
 const colors = require("colors")
+const {NodeSSH} = require('node-ssh')
+const ssh = new NodeSSH()
+const phoneRegex = /^1?([1-9])(\d{9})/
 
 // Init Discord
 const client = new Discord.Client({intents: ["Guilds"]})
@@ -27,12 +30,17 @@ client.on("ready", () => {
 	(async () => {
 		try {
 			console.log(`${colors.cyan("[INFO]")} Registering Commands...`)
-			await rest.put(
-				Routes.applicationCommands(config.discord.clientID), {
-					body: commands,
-				}
-			);
-			console.log(`${colors.cyan("[INFO]")} Successfully registered commands.`);
+			let start = Date.now()
+			// For every guild
+			for (const guild of client.guilds.cache.values()) {
+				// Register commands
+				await rest.put(
+					Routes.applicationGuildCommands(client.user.id, guild.id), {
+						body: commands
+					},
+				);
+			}
+			console.log(`${colors.cyan("[INFO]")} Successfully registered commands. Took ${colors.green((Date.now() - start) / 1000)} seconds.`);
 		} catch (error) {
 			console.error(error);
 		}
@@ -40,9 +48,41 @@ client.on("ready", () => {
 
 });
 
-// Init
+client.on("interactionCreate", async interaction => {
+	if (!interaction.isCommand()) return;
+	switch(interaction.commandName) {
+		case "fax": // Allows a user to send a PDF to a fax number
+			if (!interaction.options.get("to").value.toString().match(phoneRegex)) return interaction.reply({content: "Invalid phone number.", ephemeral: true});
+			if (interaction.options.get("to").value !== interaction.options.get("confirm").value) return interaction.reply({content: "Phone numbers do not match.", ephemeral: true});
+			if (!interaction.options.get("file").value) return interaction.reply({content: "No file provided, how'd you even do that???", ephemeral: true});
+			if (interaction.options.get("file").attachment.contentType != "application/pdf") return interaction.reply({content: "File is not a PDF.", ephemeral: true});
+			interaction.reply({content: "Getting Ready...", ephemeral: true}).then(async () => {
+
+				// Start doing SSH stuff
+				await ssh.execCommand("mkdir -p /tmp/fax/send")
+				await ssh.execCommand(`wget -O /tmp/fax/send/${interaction.options.get("to").value}.pdf ${interaction.options.get("file").attachment.url}`)
+				await ssh.execCommand(`cd /tmp/fax/send && gs -q -dNOPAUSE -dBATCH -sDEVICE=tiffg4 -sPAPERSIZE=letter -sOutputFile=${interaction.options.get("to").value}.tiff ${interaction.options.get("to").value}.pdf`)
+				await ssh.execCommand(`chown asterisk:asterisk /tmp/fax/send/${interaction.options.get("to").value}.tiff`)
+				await ssh.execCommand(`rm /tmp/fax/send/${interaction.options.get("to").value}.pdf`)
+				interaction.editReply({content: "Sending...", ephemeral: true});
+				await ssh.execCommand(`/var/lib/asterisk/bin/callback ${interaction.options.get("to").value} sendfax.s.1 0 0 ${config.fax.callerId}`);
+			});
+			break;
+	}
+});
+
+// Init SSH and connect to Discord
 console.log(`${colors.cyan("[INFO]")} Starting...`)
 // Start timer to see how long startup takes
 const initTime = Date.now()
-// Login
-client.login(config.discord.token)
+ssh.connect(config.ssh).then(() => {
+	console.log(`${colors.cyan("[INFO]")} Connected to SSH server.`)
+	client.login(config.discord.token)
+})
+
+// Take over Ctrl+C
+process.on('SIGINT', function() {
+	console.log(`${colors.cyan("[INFO]")} Shutting down...`)
+	ssh.dispose()
+	process.exit()
+});
